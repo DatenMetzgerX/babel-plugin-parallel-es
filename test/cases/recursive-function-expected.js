16bfd18f42376732c7d68023a4a3ca37e018f84c
Micha Reiser <micha@famreiser.ch>
2016-11-28 19:10:06 +0100

    Support Extracting Recursive Functions

---
+++ test/cases/recursive-function-expected.js
@@ -0,0 +1,15 @@
+import parallel from "parallel-es";
+
+function fibonacci(value) {
+    if (value <= 2) {
+        return 1;
+    }
+    return fibonacci(value - 1) + fibonacci(value - 2);
+}
+
+export function transformData(data) {
+    return parallel.from(data).map({
+        identifier: "static:recursive-function-case.js/fibonacci",
+        _______isFunctionId: true
+    });
+}
