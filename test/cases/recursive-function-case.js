1d5526f566624d1a6cfa93d16943270dd8642705
Micha Reiser <micha@famreiser.ch>
2016-12-03 18:15:09 +0100

    Update Dependencies

--- test/cases/recursive-function-case.js
+++ test/cases/recursive-function-case.js
@@ -1,12 +1,16 @@
 import parallel from "parallel-es";
 
+const terminalValue = 1;
+
 function fibonacci(value) {
     if (value <= 2) {
-        return 1;
+        return terminalValue;
     }
     return fibonacci(value - 1) + fibonacci(value - 2);
 }
 
 export function transformData(data) {
-    return parallel.from(data).map(fibonacci);
+    return parallel.from(data).map(function (value) {
+        return fibonacci(value);
+    });
 }
--- test/cases/recursive-function-expected.js
+++ test/cases/recursive-function-expected.js
@@ -1,15 +1,23 @@
 import parallel from "parallel-es";
 
+const terminalValue = 1;
+
 function fibonacci(value) {
     if (value <= 2) {
-        return 1;
+        return terminalValue;
     }
     return fibonacci(value - 1) + fibonacci(value - 2);
 }
 
 export function transformData(data) {
-    return parallel.from(data).map({
-        identifier: "static:recursive-function-case.js/fibonacci",
+    function _environmentExtractor() {
+        return {
+            terminalValue: terminalValue
+        };
+    }
+
+    return parallel.from(data).inEnvironment(_environmentExtractor()).map({
+        identifier: "static:recursive-function-case.js/_entry_anonymous",
         _______isFunctionId: true
     });
 }
